0addb658f6607eb87cb83f77f2940cd1d6717945
Niklas Bauer <nikbau2000@gmail.com>
2022-08-04 11:29:09 +0200

    initial commit, dense Cholesky solver

---
+++ cholesky.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <pybind11/pybind11.h>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+
+namespace py = pybind11;
+using Eigen::Map;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::Matrix;
+
+Eigen::VectorXd solveSpLLT(const Eigen::SparseMatrix<double>& A, const Eigen::VectorXd& rhs){
+    Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> C(A);
+    return C.solve(rhs);
+}
+
+//Exposable function wrapper for solveLLT
+void solverDense(void *out, void **in){
+    //A, rhs, rhs.shape
+    //Parse Inputs
+    auto* A_ptr = reinterpret_cast<double *>(in[0]);
+    auto* rhs_ptr = reinterpret_cast<double *>(in[1]);
+    auto* out_ptr = reinterpret_cast<double *>(out);
+    auto n = *reinterpret_cast<const std::int64_t*>(in[2]);
+
+    MatrixXd A = Map<const MatrixXd>(A_ptr, n, n);
+    VectorXd rhs = Map<const VectorXd>(rhs_ptr, n, 1);
+
+    VectorXd res = A.llt().solve(rhs); //Cholesky: A has to be symmetrical positive definite!
+    Map<VectorXd>(out_ptr, n, 1) = res;
+}
+
+//Fill pybind dictionary
+pybind11::dict Registrations() {
+  pybind11::dict dict;
+  dict["solverDense"] = pybind11::capsule((void*)solverDense, "xla._CUSTOM_CALL_TARGET");
+  return dict;
+}
+
+// Expose functions in Registrations dictionary
+PYBIND11_MODULE(choleskyEigenLib, m) { m.def("registrations", &Registrations); }
